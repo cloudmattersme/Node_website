e0921c38cc2364add5a9f20b46e6346402917031
cloudmattersme <33652215+cloudmattersme@users.noreply.github.com>
2020-03-16 14:40:04 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 api_response=`curl -s -H 'content-type: application/json'   https://scan.api.redlock.io/v1/iac   --data-binary "@cloudformation_ecs.yaml"`
 rules_matched_count=`jq '.result.rules_matched|length' <<< $api_response`
-echo "rules matched count: $rules_matched_count";
+echo "Prisma Cloud Policy Violation count: $rules_matched_count";
 
 if [ "$rules_matched_count" -ne 0 ];then
-  echo "Build failed with error code $rules_matched_count because the following checks failed:";
+  echo "Build failed with Violation Count  $rules_matched_count because the following Prisma Cloud Policies failed:";
   jq '.result.rules_matched' <<< $api_response
 fi
 
